58d61474060dcb2a58710eaeae6dd708f680ebda
Phani Swathi <71266027+nuluphaniswathi@users.noreply.github.com>
2024-11-11 19:19:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import "./index.css";
+import Chatbot from "./ChatBot";
+import { useState, useEffect } from "react";
+import { IoChatboxEllipsesSharp } from "react-icons/io5";
+
+function ChatBotUI() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const userId = user?.id
+  const [isChatbotClicked, setIsChatbotClicked] = useState(false);
+  const [sessionId, setSessionId] = useState(`session_${userId || "guest"}_${Date.now()}`);
+  console.log("sessionId",sessionId)
+  const toggle = () => {
+    if(!isChatbotClicked){
+      setSessionId(`session_${userId || "guest"}_${Date.now()}`)
+    }
+    setIsChatbotClicked(!isChatbotClicked);
+  };
+  useEffect(() => {
+    if(isChatbotClicked){
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isChatbotClicked]);
+  return (
+    <div className="bot-ui">
+      {!isChatbotClicked && (
+        <button className="chatbot-icon-button" onClick={toggle}>
+          <IoChatboxEllipsesSharp size={25}/>
+        </button>
+      )}
+      {isChatbotClicked && (<Chatbot toggle={toggle} sessionId={sessionId}/>)}
+    </div>
+  );
+}
+
+export default ChatBotUI;
